9194ac5bab87df24d13fc71aae1b105f0732de71
ecyates <ecyates1@mac.com>
2025-04-23 21:42:23 -0700

    Merging FE/BE

--- safespace-backend/app/models.py
+++ safespace-backend/app/models.py
@@ -12,12 +12,12 @@ class Account(db.Model):
     
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(unique=True, nullable=False)
-    firebase_uid: Mapped[str] = mapped_column(unique=True)
+    firebase_uid: Mapped[str] = mapped_column(unique=True, nullable=False)
     role: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(default=True)
     created_at: Mapped[datetime] = mapped_column(nullable=False, default=datetime.now)
-    updated_at: Mapped[datetime] = mapped_column(nullable=False, default=datetime.now, onupdate=datetime.now)
-    last_login_at: Mapped[datetime] = mapped_column(nullable=True)
+    last_login: Mapped[datetime] = mapped_column(nullable=False, default=datetime.now, onupdate=datetime.now)
+    email_verified: Mapped[bool] = mapped_column(default=True)
     
     user: Mapped['User'] = db.relationship(back_populates="account", uselist=False)
 
@@ -30,6 +30,7 @@ class User(db.Model):
     first_name: Mapped[str] = mapped_column(nullable=False)
     last_name: Mapped[str] = mapped_column(nullable=False)
     created_at: Mapped[datetime] = mapped_column(nullable=False, default=datetime.now)
+    updated_at: Mapped[datetime] = mapped_column(nullable=False, default=datetime.now)
     # the code below is commented out for possible use in future implementations
     # street_address: Mapped[str] = mapped_column(nullable=False)
     # city: Mapped[str] = mapped_column(nullable=False)
