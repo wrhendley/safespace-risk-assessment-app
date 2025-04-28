25940dcaaf4baf41599693fce1a550a161a54653
Wesley Hendley <wrhendley@gmail.com>
2025-04-28 13:22:58 -0500

    Add flask-cors

--- safespace-backend/app/extensions.py
+++ safespace-backend/app/extensions.py
@@ -3,6 +3,7 @@ from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from flask_caching import Cache
 from flask_migrate import Migrate
+from flask_cors import CORS
 
 ma = Marshmallow()
 limiter = Limiter(key_func=get_remote_address,
@@ -11,3 +12,5 @@ limiter = Limiter(key_func=get_remote_address,
 cache = Cache()
 
 migrate = Migrate()
+
+cors = CORS()
