30b9e98696e80e85f74c5c1782019263d84c6df6
Wesley Hendley <wrhendley@gmail.com>
2025-05-05 15:04:24 -0500

    Add update_risk_assessment route

--- safespace-backend/app/blueprints/risk_assessments/routes.py
+++ safespace-backend/app/blueprints/risk_assessments/routes.py
@@ -27,3 +27,30 @@ def create_risk_assessment():
         db.session.commit()
     
     return risk_assessment_schema.jsonify(new_risk_assessment), 201
+
+@risk_assessments_bp.route('/', methods=['PUT'], strict_slashes=False)
+@limiter.limit("5 per minute")
+@auth_required
+def update_risk_assessment():
+    account = g.account
+    user = User.query.filter_by(account_id=account.id).first()
+    
+    try:
+        risk_assessment_data = risk_assessment_schema.load(request.json)
+    except ValidationError as e:
+        return jsonify(e.messages), 400
+    
+    risk_assessment = RiskAssessment.query.filter_by(id=risk_assessment_data.get('id')).first()
+    if not risk_assessment:
+        return jsonify({"message": "Risk assessment not found"}), 404
+    
+    try:
+        for key, value in risk_assessment_data.items():
+            setattr(risk_assessment, key, value)
+        
+        db.session.commit()
+        
+        return risk_assessment_schema.jsonify(risk_assessment), 200
+    
+    except Exception as e:
+        return jsonify({"message": f'There was an error: {str(e)}'}), 400
