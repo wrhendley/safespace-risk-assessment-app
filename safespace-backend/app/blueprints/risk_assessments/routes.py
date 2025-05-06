6a00a3b672a8f517db6dc8a91ab544e03a9efa23
Wesley Hendley <wrhendley@gmail.com>
2025-05-05 19:54:54 -0500

    Add get_risk_assessments

--- safespace-backend/app/blueprints/risk_assessments/routes.py
+++ safespace-backend/app/blueprints/risk_assessments/routes.py
@@ -7,6 +7,10 @@ from marshmallow import ValidationError
 from app.models import Account, User, RiskAssessment, db
 from sqlalchemy import select
 
+DEFAULT_PAGE = 1
+DEFAULT_PER_PAGE = 10
+MAX_PER_PAGE = 100
+
 @risk_assessments_bp.route('/', methods=['POST'], strict_slashes=False)
 @limiter.limit("5 per minute")
 @auth_required
@@ -51,6 +55,35 @@ def update_risk_assessment():
         db.session.commit()
         
         return risk_assessment_schema.jsonify(risk_assessment), 200
+    
+    except Exception as e:
+        return jsonify({"message": f'There was an error: {str(e)}'}), 400
+
+@risk_assessments_bp.route('/', methods=['GET'], strict_slashes=False)
+@limiter.limit("5 per minute")
+@auth_required
+def get_risk_assessments():
+    account = g.account
+    user = User.query.filter_by(account_id=account.id).first()
+    
+    if not user:
+        return jsonify({"message": "User not found"}), 404
+    
+    try:
+        page = request.args.get('page', DEFAULT_PAGE, type=int)
+        per_page = min(request.args.get('per_page', DEFAULT_PER_PAGE, type=int), 100)
+        if page < 1 or per_page < 1:
+            return jsonify({"message": "Invalid page or per_page parameter"}), 400
+        
+        risk_assessments = select(RiskAssessment).filter(RiskAssessment.users.any(id=user.id)).order_by(RiskAssessment.assessment_date.desc())
+        risk_assessments = db.paginate(risk_assessments, page=page, per_page=per_page).items
+        if not risk_assessments:
+            return jsonify({"message": "No risk assessments found"}), 404
+        
+        return risk_assessments_schema.jsonify(risk_assessments), 200
+    
+    except ValidationError as e:
+        return jsonify(e.messages), 400
     
     except Exception as e:
         return jsonify({"message": f'There was an error: {str(e)}'}), 400
