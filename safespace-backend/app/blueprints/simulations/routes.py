dda8d67f4dffc61f91d5381a290666e1f442cf24
Wesley Hendley <wrhendley@gmail.com>
2025-05-16 13:16:32 -0500

    Update route to validate and prepare data for saving an investment assessment

--- safespace-backend/app/blueprints/simulations/routes.py
+++ safespace-backend/app/blueprints/simulations/routes.py
@@ -5,19 +5,32 @@ from app.utils.util import auth_required
 from app.models import User, db
 from marshmallow import ValidationError
 from sqlalchemy import select
+from datetime import datetime
 
 @simulations_bp.route("/investments", methods=["POST"], strict_slashes=False)
 @auth_required
 def save_portfolio_simulation():
     data = request.get_json()
-    print("Received data:", data)
     
     account = g.account
-    query = select(User).where(User.account_id == account.id)
-    user = db.session.execute(query).scalars().first()
+    user = db.session.execute(
+        select(User).where(User.account_id == account.id)
+    ).scalar_one_or_none()
+    
+    print("start_date", data["start_date"])
+    print("end_date", data["end_date"])
+    data["user_id"] = user.id
+    
+    print (g.account.id)
+    print("Received data:", data)
+    for field in investment_schema.fields:
+        print(field, data.get(field))
+    
+    if not user:
+        return jsonify({"error": "User not found"}), 404
     
     try:
-        data = investment_schema.load(request.get_json())
+        validated_data = investment_schema.load(request.get_json())
     except ValidationError as err:
         return jsonify(err.messages), 400
     return jsonify(data), 200
