e2cc36bd9e5760ce447164f1114f5ca184c1e031
Wesley Hendley <wrhendley@gmail.com>
2025-05-17 19:48:20 -0500

    Add rate limiting to investment and loan simulation endpoints

--- safespace-backend/app/blueprints/simulations/routes.py
+++ safespace-backend/app/blueprints/simulations/routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request, jsonify, g
 from app.blueprints.simulations import simulations_bp
 from app.blueprints.simulations.schemas import loan_schema, investment_schema, asset_schema
 from app.utils.util import auth_required
+from app.extensions import limiter
 from app.models import User, db, InvestmentRiskAssessment, Asset, LoanRiskAssessment
 from marshmallow import ValidationError
 from sqlalchemy import select
@@ -9,6 +10,7 @@ from datetime import datetime
 
 @simulations_bp.route("/investments", methods=["POST"], strict_slashes=False)
 @auth_required
+@limiter.limit("1 per 10 seconds")
 def save_portfolio_simulation():
     data = request.get_json()
     ticker_data = data.pop("ticker_data", [])
@@ -50,6 +52,7 @@ def save_portfolio_simulation():
 
 @simulations_bp.route("/loans", methods=["POST"], strict_slashes=False)
 @auth_required
+@limiter.limit("1 per 10 seconds")
 def save_loan_simulation():
     data = loan_schema.load(request.get_json())
     
