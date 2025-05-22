13f46cd4e30bcf17246bd799c5e773ee7c2a7a2c
Wesley Hendley <wrhendley@gmail.com>
2025-05-22 10:46:06 -0500

    Add route to delete a loan risk assessment

--- safespace-backend/app/blueprints/simulations/routes.py
+++ safespace-backend/app/blueprints/simulations/routes.py
@@ -196,3 +196,30 @@ def get_loan_simulation(loan_id):
         return jsonify({"error": "User not authorized to access this simulation"}), 403
     
     return jsonify(loan_schema.dump(loan_risk_assessment)), 200
+
+@simulations_bp.route("/loans/<int:loan_id>", methods=["DELETE"], strict_slashes=False)
+@auth_required
+@limiter.limit("1 per 10 seconds")
+def delete_loan_simulation(loan_id):
+    account = g.account
+    user = db.session.execute(
+        select(User).where(User.account_id == account.id)
+    ).scalar_one_or_none()
+    
+    if not user:
+        return jsonify({"error": "User not found"}), 404
+    
+    loan_risk_assessment = db.session.execute(
+        select(LoanRiskAssessment)
+        .where(LoanRiskAssessment.id == loan_id)
+    ).scalar_one_or_none()
+    
+    if not loan_risk_assessment:
+        return jsonify({"error": "Loan simulation not found"}), 404
+    if user.id not in [user.id for user in loan_risk_assessment.users]:
+        return jsonify({"error": "User not authorized to access this simulation"}), 403
+    
+    db.session.delete(loan_risk_assessment)
+    db.session.commit()
+    
+    return jsonify({"message": "Loan simulation deleted successfully"}), 200
